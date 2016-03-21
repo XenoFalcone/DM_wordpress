21f38c7177a0c17d58a937ff106f95efa94c86e4
Ydnac <candy.gnt@gmail.com>
2016-03-21 15:30:04 +0100

    starter-child

--- wordpress_DM/wp-content/themes/starter/lib/metaboxes.php
+++ wordpress_DM/wp-content/themes/starter/lib/metaboxes.php
@@ -21,3 +21,37 @@ if ( file_exists(get_template_directory() . '/cmb2/init.php' ) ) {
 	require_once get_template_directory() . '/CMB2/init.php';
 }
 
+/*add_action ( 'cmb2_admin_init', 'sejour_metabox' );
+function sejour_metabox() {
+	$prefix = '_sejour';
+
+	$cmb_musique_concert_artist = new_cmb2_box ( array (
+			'id' => $prefix . 'metabox',
+			'title' => __ ( 'Concert Informations', 'musique' ),
+			'object_types' => array (
+					'concert'
+			)
+	) );
+
+	$group_field_id = $cmb_musique_concert_artist->add_field ( array (
+			'id' => $prefix . 'demo',
+			'type' => 'group',
+			'description' => __ ( 'Generates reusable form entries', 'cmb2' ),
+			'options' => array (
+					'group_title' => __ ( 'Entry {#}', 'cmb2' ), // {#} gets replaced by row number
+					'add_button' => __ ( 'Add Another Entry', 'cmb2' ),
+					'remove_button' => __ ( 'Remove Entry', 'cmb2' ),
+					'sortable' => true
+			) // beta
+			                         // 'closed' => true, // true to have the groups closed by default
+
+	) );
+
+	$cmb_musique_concert_artist->add_group_field ( $group_field_id, array (
+			'id' => $prefix . 'artist',
+			'name' => __ ( 'Artiste concert', 'musique' ),
+			'type' => 'select',
+			'options' => 'get_artists_array'
+	) );
+
+}*/
